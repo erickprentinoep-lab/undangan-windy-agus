e9481ae0df7f89939523dec2e8fe1e161604fc60
Erick Prentinoep <erickprentinoep@gmail.com>
2026-06-09 12:43:39 +0700

    Fix: scroll reveal animation, carousel overflow, music update

--- script.js
+++ script.js
@@ -460,8 +460,8 @@ function initScrollReveal() {
             }
         });
     }, {
-        threshold: 0.1,
-        rootMargin: "0px 0px -40px 0px"
+        threshold: 0.05,
+        rootMargin: "0px 0px 0px 0px"
     });
 
     revealElements.forEach(el => {
